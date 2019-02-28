0cb42c84e8ab87b01a280ac0bbadbca6bbac9e22
Raoul Theunis <raoul.theunis@cmtelecom.com>
2019-02-28 20:47:52 +0100

    first commit of led api

---
+++ CATToTheLED.Web.Api/Controllers/LEDController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Microsoft.AspNetCore.Mvc;
+using ws281x.Net;
+
+namespace CATToTheLED.Web.Api.Controllers
+{
+    [Route("ledstrip/v1")]
+    [ApiController]
+    public class LedController : ControllerBase
+    {
+        private readonly Neopixel _neopixel;
+
+        public LedController()
+        {
+            _neopixel = new Neopixel(60, 18);
+        }
+
+        [HttpGet("color")]
+        public ActionResult<Dictionary<int, Color>> Get()
+        {
+            Dictionary<int, Color> colorIndex = new Dictionary<int, Color>();
+
+            for(int i = 0; i <= 60; i++)
+                colorIndex.Add(i, _neopixel.LedList.GetColor(i));
+
+            return colorIndex;
+        }
+
+        [HttpGet("led/{ledIndex}/color")]
+        public ActionResult<Color> Get(int ledIndex)
+        {
+            return _neopixel.LedList.GetColor(ledIndex);
+        }
+
+        [HttpPut("led/{ledIndex}/setColor")]
+        public void SetColor(int ledIndex, Color color)
+        {
+            _neopixel.SetPixelColor(ledIndex, color);
+        }
+    }
+}
---
+++ CATToTheLED.Web.Api/Startup.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace CATToTheLED.Web.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info {Title = "Platform DeskPro", Version = "v1"});
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Platform DeskPro v1.0");
+                c.RoutePrefix = "docs";
+            });
+
+            app.UseMvc();
+        }
+    }
+}
