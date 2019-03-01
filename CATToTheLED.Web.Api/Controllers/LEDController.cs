30cbee8b95c0da099cb8dcf01b51b5e051f86aa0
Jan Willem Raats <jw.raats@gmail.com>
2019-03-01 12:40:34 +0100

    Use it as a contructor the neoPixel

--- CATToTheLED.Web.Api/Controllers/LEDController.cs
+++ CATToTheLED.Web.Api/Controllers/LEDController.cs
@@ -12,43 +12,35 @@ namespace CATToTheLED.Web.Api.Controllers
     {
         private readonly int _ledCount = 60;
         private readonly int _pin = 18;
+        private readonly Neopixel _neopixel;
+
+        public LedController(){
+            _neopixel = new ws281x.Net.Neopixel(ledCount: _ledCount, pin: _pin);
+            _neopixel.Begin();
+        }
+
         [HttpGet("color")]
         public Dictionary<int, string> GetColors()
         {
-            // You can also choose a custom color order
-            var neopixel = new ws281x.Net.Neopixel(ledCount: _ledCount, pin: _pin);
-
-            // Always initialize the wrapper first
-            neopixel.Begin();
-
             Dictionary<int, string> colorIndex = new Dictionary<int, string>();
-            for (var i = 0; i < neopixel.GetNumberOfPixels(); i++)
+            for (var i = 0; i < _neopixel.GetNumberOfPixels(); i++)
             {
-                colorIndex.Add(i, $"R:{neopixel.LedList.GetColor(i).G}, G:{neopixel.LedList.GetColor(i).B}, {neopixel.LedList.GetColor(i).R}");
+                colorIndex.Add(i, _neopixel.LedList.GetColor(i).Name);
             }
-
             return colorIndex;
         }
 
         [HttpPut("color")]
         public void SetColors(string colorString)
         {
-
-            // You can also choose a custom color order
-            var neopixel = new ws281x.Net.Neopixel(ledCount: _ledCount, pin: _pin);
-
-            // Always initialize the wrapper first
-            neopixel.Begin();
-
-            for (var i = 0; i < neopixel.GetNumberOfPixels(); i++)
+            for (var i = 0; i < _neopixel.GetNumberOfPixels(); i++)
             {
                 Color desiredColor = System.Drawing.Color.FromName(colorString);
-                neopixel.SetPixelColor(i, System.Drawing.Color.FromArgb(desiredColor.G, desiredColor.B, desiredColor.R));
-                neopixel.LedList.SetColor(i, System.Drawing.Color.FromArgb(desiredColor.G, desiredColor.B, desiredColor.R));
+                _neopixel.LedList.SetColor(i, System.Drawing.Color.FromArgb(desiredColor.G, desiredColor.B, desiredColor.R));
             }
 
             // Apply changes to the led
-            neopixel.Show();
+            _neopixel.Show();
 
         }
 
@@ -61,19 +53,13 @@ namespace CATToTheLED.Web.Api.Controllers
         [HttpGet("led/{ledIndex}/color")]
         public string GetColor(int ledIndex)
         {
-            var neopixel = new ws281x.Net.Neopixel(ledCount: _ledCount, pin: _pin);
-
-            if (!(ledIndex >= 0 && ledIndex < neopixel.GetNumberOfPixels()))
+            if (!(ledIndex >= 0 && ledIndex < _neopixel.GetNumberOfPixels()))
             {
                 throw new Exception("Led is not existing");
             }
 
-
-            // Always initialize the wrapper first
-            neopixel.Begin();
-
             //Get the attribute
-            var color = neopixel.LedList.GetColor(ledIndex).Name;
+            var color = _neopixel.LedList.GetColor(ledIndex).Name;
 
             return color;
         }
@@ -81,23 +67,16 @@ namespace CATToTheLED.Web.Api.Controllers
         [HttpPut("led/{ledIndex}/color")]
         public void SetColor(int ledIndex, string colorString)
         {
-
-            // You can also choose a custom color order
-            var neopixel = new ws281x.Net.Neopixel(ledCount: _ledCount, pin: _pin);
-
-
-            if (!(ledIndex >= 0 && ledIndex < neopixel.GetNumberOfPixels()))
+        
+            if (!(ledIndex >= 0 && ledIndex < _neopixel.GetNumberOfPixels()))
             {
                 throw new Exception("Led is not existing");
             }
-            // Always initialize the wrapper first
-            neopixel.Begin();
             Color desiredColor = System.Drawing.Color.FromName(colorString);
-            neopixel.SetPixelColor(ledIndex, System.Drawing.Color.FromArgb(desiredColor.G, desiredColor.B, desiredColor.R));
-            neopixel.LedList.SetColor(ledIndex, System.Drawing.Color.FromArgb(desiredColor.G, desiredColor.B, desiredColor.R));
+            _neopixel.LedList.SetColor(ledIndex, System.Drawing.Color.FromArgb(desiredColor.G, desiredColor.B, desiredColor.R));
 
             // Apply changes to the led
-            neopixel.Show();
+            _neopixel.Show();
 
         }
 
